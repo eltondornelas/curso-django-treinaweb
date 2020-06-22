187453464deba46e2842a66c4cafa567992241d1
Elton Dornelas <eltondornelas@gmail.com>
2020-06-22 19:43:46 -0300

    Criando o model Client

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    SEX_CHOICES = (
+        ('F', 'Feminino'),
+        ('M', 'Masculino'),
+        ('N', 'Nenhuma das opções')
+    )
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    birthday = models.DateField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    profession = models.CharField(max_length=50, null=False, blank=False)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, null=False,
+                           blank=False)
+
+    def __str__(self):
+        return self.name
