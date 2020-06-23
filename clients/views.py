26501525e545b6ff318c6bae1bcc2b66186b294d
Elton Dornelas <eltondornelas@gmail.com>
2020-06-22 22:13:13 -0300

    Criando form para cadastro de cliente

--- clients/views.py
+++ clients/views.py
@@ -9,5 +9,21 @@ def list_clients(request):
 
 
 def register_client(request):
-    form = ClientForm()
+    """
+    # ñ precisa do action do form que ele esta renderizado por este método
+        logo ele sabe que o submit é um request para este método
+    # csrf_token é um hash que garante que este form está sendo recebido por
+        uma aplicação que ele conhece do django
+    """
+
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    else:
+        # sem o else ele cria um novo form e perde os erros guardados da
+        # instancia que não foi validada
+        form = ClientForm()
+
     return render(request, 'clients/form_client.html', {'form': form})
