e1a36061290137309eaf71c3ef3e5693eac08905
Elton Dornelas <eltondornelas@gmail.com>
2020-06-28 22:47:20 -0300

    Entendo o prefetch_related e incluindo produtos na listagem do pedido

--- clients/services/order_service.py
+++ clients/services/order_service.py
@@ -18,10 +18,15 @@ def register_order(order):
 def list_orders():
     # orders = Order.objects.all()
     orders = Order.objects.select_related('client').all()
-    # client -> o nome da relação
+    # # client -> o nome da relação; select_related é para 1-N ou 1-1
+    # orders = Order.objects.prefetch_related('products').all()
 
-    for i in orders:
-        print(i.client.name)
+    # for i in orders:  # exemplo de pedido com produto N-N (N+1 problem)
+        # # print(i.products.name) # qnd coloca o prefetch da erro
+        #  print(i.products.all())
+
+    # for i in orders:  # exemplo de cliente com pedido 1-N (N+1 problem)
+    #     print(i.client.name)
         # for igual ao do template para mostrar quantas queries fazemos
 
     # para evitar o problema do N+1, vamos utilizar o join com select_related
@@ -33,6 +38,16 @@ def list_orders():
 
 def list_order_id(id):
     order = Order.objects.get(id=id)
+
+    # aqui ñ precisa do prefetch pois como já passamos o id, ele já faz o join
+    # aqui não precisa desse cuidado, pois é específico
+    # o select e o prefetch é quando vai precisar do "todo"
+    for i in order.products.all():
+        print(i.name)
+
+    print(connection.queries)
+    print(len(connection.queries))
+
     return order
 
 
